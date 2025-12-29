e2070b005652fd36f296720f9b6ed23222da415c
huynggg <huyanguyen3695@gmail.com>
2025-12-28 23:17:37 -0600

    no smearing effects

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -114,8 +114,8 @@ return {
     opts = {
       stiffness = 0.5,
       trailing_stiffness = 0.5,
-      damping = 0.67,
       matrix_pixel_threshold = 0.5,
+      -- damping = 0.67,
     },
   },
 }
